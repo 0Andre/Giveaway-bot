db589516aa363d295e64a605fb6aa8b5df934521
Andre <89987199+0Andre@users.noreply.github.com>
2022-01-30 16:44:51 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -21,13 +21,3 @@ module.exports.run = async (client, message, args) => {
 module.exports.help = {
     name: "invite"
 }
-
-/*
-
-=======================================================
-|              Coded by: T.F.A#1887                   |
-|  Do not remove credits while sharing this project!  |
-|         You will get a copyright warning            |
-=======================================================
-
-*/

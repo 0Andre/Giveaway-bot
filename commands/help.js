7bd00a22df7389e50af5feb43ee6371c0af809b3
Andre <89987199+0Andre@users.noreply.github.com>
2022-01-30 16:44:31 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -36,13 +36,3 @@ module.exports.run = async (client, message, args) => {
 module.exports.help = {
   name: "help"
 }
-
-/*
-
-=======================================================
-|              Coded by: T.F.A#1887                   |
-|  Do not remove credits while sharing this project!  |
-|         You will get a copyright warning            |
-=======================================================
-
-*/
